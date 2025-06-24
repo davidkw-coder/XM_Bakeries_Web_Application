a3a5aad182048cb8e20fe5f00d74eeb56130be10
kid coder <kidcoderman@gmail.com>
2025-06-24 12:52:48 +0200

    Real and Running Commit

--- server.js
+++ server.js
@@ -46,6 +46,3 @@ app.use((req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
-
